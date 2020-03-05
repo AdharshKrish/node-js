65b22dc8714940c89d91f0d09b996b482381080d
Adharsh <adharshkrish@outlook.com>
2020-03-05 11:10:33 +0530

    http server

--- server tutorial/server.js
+++ server tutorial/server.js
@@ -1,9 +1,19 @@
 const http = require('http')
+const fs = require('fs')
 const port = 3000
 
 const server = http.createServer(function (req, res) {
-    res.write('Hello Node')
-    res.end()
+    // res.write('Hello Node')
+    res.writeHead(200, { 'Content-Type': 'text/html' })
+    fs.readFile('index.html', function (err,data){
+        if(err){
+            res.writeHead(404)
+            res.write('Error: ' + err.message)
+        } else{
+            res.write(data)
+        }
+        res.end()
+    })
 })
 
 server.listen(port, function (error) {
